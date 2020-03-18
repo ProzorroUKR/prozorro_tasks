018bec78c8634b28d71ea00d631fbba16a322696
smithumble <smithumble@gmail.com>
2020-03-18 15:31:59 +0200

    Add liqpay sndbox configuration. Fix checkout request params validation.

--- liqpay_int/exceptions.py
+++ liqpay_int/exceptions.py
@@ -1,8 +1,27 @@
 from werkzeug.exceptions import HTTPException
 
 
+class LiqpayResponseFailureError(HTTPException):
+    code = 400
+    description = (
+        "Liqpay response failure"
+    )
+
+
+
 class LiqpayResponseError(HTTPException):
     code = 400
     description = (
         "Liqpay response error"
     )
+
+    def __init__(self, description=None, response=None, liqpay_err_description=None):
+        if liqpay_err_description is not None:
+            description = "{}: {}".format(
+                description or LiqpayResponseError.description,
+                liqpay_err_description
+            )
+        super(LiqpayResponseError, self).__init__(
+            description=description,
+            response=response
+        )
