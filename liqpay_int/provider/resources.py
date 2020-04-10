32f4107d9b8d0d00cf70ff5931d85982616ee8b2
smithumble <smithumble@gmail.com>
2020-04-10 14:54:14 +0300

    Payment push 503

--- liqpay_int/provider/resources.py
+++ liqpay_int/provider/resources.py
@@ -1,8 +1,9 @@
+from flask_restx import abort
 from kombu.exceptions import OperationalError
+from pymongo.errors import PyMongoError
 
 from app.auth import ip_group_required, get_network_data
 from app.logging import getLogger
-from liqpay_int.exceptions import ProzorroApiHTTPException
 from liqpay_int.provider.models import model_payment
 from liqpay_int.provider.namespaces import api
 from liqpay_int.resources import Resource
@@ -27,17 +28,22 @@ class PushResource(Resource):
     @api.marshal_with(model_response_success, code=200)
     @api.response(400, 'Bad Request', model_response_detailed_error)
     @api.response(403, 'Forbidden', model_response_error)
+    @api.response(503, 'Service Unavailable', model_response_failure)
     @api.response(500, 'Internal Server Error', model_response_failure)
     @api.expect(model_payment, validate=True)
     def post(self):
-        save_payment_item(api.payload, (get_network_data() or {}).get("username"))
         extra = {"PAYMENT_DESCRIPTION": api.payload.get("description")}
+        try:
+            save_payment_item(api.payload, (get_network_data() or {}).get("username"))
+            logger.error("Payment save failed.", extra=extra)
+        except PyMongoError:
+            abort(code=503)
         logger.info("Payment push received.", extra=extra)
         try:
             process_payment_data.apply_async(kwargs=dict(
                 payment_data=api.payload
             ))
         except (OperationalError):
-            logger.error("Payment processing task failed.", extra=extra)
-            raise ProzorroApiHTTPException()
+            logger.error("Payment send task failed.", extra=extra)
+            abort(code=503)
         return {"status": "success"}
