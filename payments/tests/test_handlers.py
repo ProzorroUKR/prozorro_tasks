885379e4518042a86914539a61cd59fc1d924efe
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2020-05-06 12:10:35 +0300

    test fix

--- payments/tests/test_handlers.py
+++ payments/tests/test_handlers.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 from payments.handlers import payments_tender_handler
+from tasks_utils.datetime import get_now
 import unittest
 
 
@@ -7,6 +8,6 @@ class TestHandlerCase(unittest.TestCase):
 
     @patch("payments.handlers.process_tender")
     def test_process_handler(self, process_tender):
-        tender = {"id": "qwa"}
+        tender = {"id": "qwa", "dateModified": get_now().isoformat()}
         payments_tender_handler(tender)
         process_tender.delay.assert_called_with(tender_id="qwa")
