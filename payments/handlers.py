6bdc6910d181d91e73028804e86c5fbab9e31411
Aleksey Kuznetsov <aleksey.kuznetsov@raccoongang.com>
2020-09-11 16:49:33 +0300

    Revert "Exclude non complaint payments from payments tender handler"
    This reverts commit c2e0f64ed8efffc67740cbb1134ebf9aa25435c7

--- payments/handlers.py
+++ payments/handlers.py
@@ -1,4 +1,3 @@
-from payments.settings import non_complaint_procedures
 from payments.tasks import process_tender
 from tasks_utils.datetime import parse_dt_string, get_now
 from environment_settings import PAYMENTS_SKIP_TENDER_DAYS
@@ -7,6 +6,5 @@ from datetime import timedelta
 
 def payments_tender_handler(tender):
     delta = get_now() - parse_dt_string(tender['dateModified'])
-    if tender['procurementMethodType'] not in non_complaint_procedures:
-        if delta < timedelta(days=PAYMENTS_SKIP_TENDER_DAYS):
-            process_tender.delay(tender_id=tender['id'])
+    if delta < timedelta(days=PAYMENTS_SKIP_TENDER_DAYS):
+        process_tender.delay(tender_id=tender['id'])
