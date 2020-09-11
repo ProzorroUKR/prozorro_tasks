c2e0f64ed8efffc67740cbb1134ebf9aa25435c7
smithumble <smithumble@gmail.com>
2020-09-11 16:45:33 +0300

    Exclude non complaint payments from payments tender handler

--- payments/handlers.py
+++ payments/handlers.py
@@ -1,3 +1,4 @@
+from payments.settings import non_complaint_procedures
 from payments.tasks import process_tender
 from tasks_utils.datetime import parse_dt_string, get_now
 from environment_settings import PAYMENTS_SKIP_TENDER_DAYS
@@ -6,5 +7,6 @@ from datetime import timedelta
 
 def payments_tender_handler(tender):
     delta = get_now() - parse_dt_string(tender['dateModified'])
-    if delta < timedelta(days=PAYMENTS_SKIP_TENDER_DAYS):
-        process_tender.delay(tender_id=tender['id'])
+    if tender['procurementMethodType'] not in non_complaint_procedures:
+        if delta < timedelta(days=PAYMENTS_SKIP_TENDER_DAYS):
+            process_tender.delay(tender_id=tender['id'])
