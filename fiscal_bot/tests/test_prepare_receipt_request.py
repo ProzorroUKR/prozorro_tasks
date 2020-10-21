baee6262ee55e5c9ca0bdcd74704c4ec93778174
Volodymyr <volodymyr@MacBook-Pro-Volodymyr.local>
2020-10-21 17:42:56 +0300

    change serializer to pickle in order to prepare_receipt_request_task object successfully serialize

--- fiscal_bot/tests/test_prepare_receipt_request.py
+++ fiscal_bot/tests/test_prepare_receipt_request.py
@@ -1,7 +1,7 @@
 from environment_settings import API_SIGN_HOST, API_SIGN_USER, API_SIGN_PASSWORD, TIMEZONE
 from tasks_utils.settings import CONNECT_TIMEOUT, READ_TIMEOUT
 from datetime import datetime
-from fiscal_bot.tasks import prepare_receipt_request
+from fiscal_bot.tasks import prepare_receipt_request, send_request_receipt
 from celery.exceptions import Retry
 from unittest.mock import patch, Mock
 import requests
@@ -105,6 +105,50 @@ class ReceiptTestCase(unittest.TestCase):
                     requests_reties=0
                 )
             )
+
+
+    @patch("fiscal_bot.tasks.build_receipt_request")
+    def test_prepare_request_call_next_task(self, build_receipt_mock):
+        build_receipt_mock.return_value = "hallo.xml", b"unencrypted contents"
+
+        supplier = dict(
+            name="Python Monty Иванович",
+            identifier="AA426097",
+            tender_id="f" * 32,
+            award_id="c" * 32,
+            tenderID="UA-2019-01-31-000147-a",
+            lot_index=None,
+        )
+
+        with patch("fiscal_bot.tasks.get_now") as get_now_mock:
+            get_now_mock.return_value = TIMEZONE.localize(datetime(2019, 4, 1, 16, 1))  # Monday 16:01
+
+            with patch("fiscal_bot.tasks.requests") as requests_mock:
+                requests_mock.post.side_effect = [
+                    Mock(
+                        status_code=200,
+                        content=b"content",
+                    ),
+                ]
+                prepare_receipt_request(supplier=supplier)
+
+            requests_mock.post.assert_called_once_with(
+                "{}/encrypt_fiscal/file".format(API_SIGN_HOST),
+                files={'file': ("hallo.xml", b"unencrypted contents")},
+                auth=(API_SIGN_USER, API_SIGN_PASSWORD),
+                timeout=(CONNECT_TIMEOUT, READ_TIMEOUT),
+            )
+
+            res = send_request_receipt.apply_async(
+                kwargs=dict(
+                    prepare_receipt_request_task=prepare_receipt_request,
+                    request_data=base64.b64encode(b"content").decode(),
+                    filename="hallo.xml",
+                    supplier=supplier,
+                    requests_reties=0
+                )
+            )
+            self.assertIsNotNone(res)
 
     @patch("fiscal_bot.tasks.send_request_receipt")
     @patch("fiscal_bot.tasks.build_receipt_request")
