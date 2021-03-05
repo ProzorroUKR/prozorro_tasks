6b23283e6b90d7f46e1b63bd5e34867eb00428d1
smithumble <smithumble@gmail.com>
2021-03-05 03:08:48 +0200

    Fix logging

--- payments/logging.py
+++ payments/logging.py
@@ -8,14 +8,14 @@ from payments.results_db import push_payment_message
 
 class PaymentResultsLoggerAdapter(logging.LoggerAdapter):
     def __init__(self, logger):
-        extra = {}
-        super(PaymentResultsLoggerAdapter, self).__init__(logger, extra)
+        super(PaymentResultsLoggerAdapter, self).__init__(logger, None)
 
-    def log(self, level, msg, *args, **kwargs):
-        extra = kwargs.setdefault("extra", self.extra)
+    def process(self, msg, kwargs):
+        extra = kwargs.setdefault("extra", self.extra or {})
         task = kwargs.pop("task", None)
         payment_data = kwargs.pop("payment_data", None)
         message_id = extra.get("MESSAGE_ID", None)
+
         if payment_data and message_id:
             try:
                 push_payment_message(payment_data, message_id, msg)
@@ -24,4 +24,5 @@ class PaymentResultsLoggerAdapter(logging.LoggerAdapter):
                     countdown = get_exponential_request_retry_countdown(task)
                     raise task.retry(countdown=countdown, exc=exc)
                 raise
-        super(PaymentResultsLoggerAdapter, self).log(level, msg, *args, **kwargs)
+
+        return msg, kwargs
